064149df4e2a52fb788f54ae27eda22bb37a8036
Praveen-1001 <21ug1139@sltc.ac.lk>
2024-01-07 00:09:12 +0530

    commit

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "prav-tfstate-bkt"
+    bucket = "chathushka-tfstate-bkt"
     key    = "terraform.tfstate"
     region = "eu-north-1"
     #dynamodb_table = "my-lock-table"
